461f7fe3581738a4c82a8cc690b024a6423c4016
johnitopaisah <johnitopaisah@gmail.com>
2024-07-28 21:40:20 +0200

    added task_2.sql

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,4 @@
 -- Show tables to verify creation
-SELECT books
-FROM information_schema.tables
-WHERE table_schema = 'alx_book_store';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'Books';
